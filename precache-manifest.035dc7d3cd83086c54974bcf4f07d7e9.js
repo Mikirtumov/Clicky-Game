c4062bbccbb995e388002835371354a1b675fb97
Mikirtumov <you@example.com>
2020-01-25 22:16:40 -0500

    Updates

---
+++ precache-manifest.035dc7d3cd83086c54974bcf4f07d7e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf79a7b184527754bb6c6c38709481b",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "16a19c8e862b800a9c43",
+    "url": "/Clicky-Game/static/css/main.c6464bc4.chunk.css"
+  },
+  {
+    "revision": "c970449d2a9ca16dd7b4",
+    "url": "/Clicky-Game/static/js/2.73d89001.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.73d89001.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16a19c8e862b800a9c43",
+    "url": "/Clicky-Game/static/js/main.479fb000.chunk.js"
+  },
+  {
+    "revision": "98d966235ec4731d9929",
+    "url": "/Clicky-Game/static/js/runtime-main.4d57dad9.js"
+  }
+]);
